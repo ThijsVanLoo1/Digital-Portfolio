647c788937c22049eb1fb52e5cef8812347e566c
ThijsVanLoo1 <thijs30042002@gmail.com>
2026-01-30 13:10:05 +0100

    Used different react routing, so Pages understands

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
-import {createBrowserRouter, RouterProvider} from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Projects from "./pages/ProjectsPage.jsx";
 import ProjectsDetailPage from "./pages/ProjectsDetailPage.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         element: <Layout />,
         children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/projects",
-                element: <Projects />,
-            },
-            {
-                path: "/projects/:slug",
-                element: <ProjectsDetailPage />,
-            },
+            { path: "/", element: <Home /> },
+            { path: "about", element: <About /> },
+            { path: "projects", element: <Projects /> },
+            { path: "projects/:slug", element: <ProjectsDetailPage /> },
         ],
     },
 ]);
@@ -33,5 +21,4 @@ function App() {
     return <RouterProvider router={router} />;
 }
 
-export default App
-
+export default App;
